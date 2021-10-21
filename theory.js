dc90f21d0a31bc5702b9bf5fa39c99726cf0adfd
Orfey-creator <romanbez96@gmail.com>
2021-10-21 14:11:58 +0300

    3 page end

---
+++ theory.js
@@ -0,0 +1,5 @@
+// document.querySelector('.themes-ul__tabs').addEventListener('click', (e) => {
+//     if (e.target.closest('.themes-ul__tab')) {
+//         e.target.querySelector('.themes-ul__tab-inner').classList.toggle('d-flex');
+//     }
+// })
